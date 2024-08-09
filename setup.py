18f89ebb593e35791ee1aaa6234561d927b40339
ryin0000 <68621031+ryin0000@users.noreply.github.com>
2024-08-09 15:32:51 -0700

    debug, updated model with addContrastAgnostic

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read requirements.txt and use it for the install_requires field
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='scripts',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Richard Yin,  Kenneth Weber',  # Add another author's name
     description='A package for muscle and fat segmentation using Python, with a GUI component.',
     url='https://github.com/MuscleMap/MuscleMap.git',
     packages=find_packages(),
-    install_requires=[
-        'monai==1.3.2',
-        'torch==2.4.0',
-        'numpy==1.24.4',
-        'nibabel==5.2.1',
-        'scikit-learn==1.3.2',
-        'pandas==2.0.3',
-        'scikit-image'
-
-    ],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'mm_segment=scripts.mm_segment:main',
