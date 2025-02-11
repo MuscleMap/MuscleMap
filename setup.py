b3b48f49c0c43d8022a9e98e5a35f9223e21049e
Kenneth Arnold Weber <kenweber@anes-nil1000.stanford.edu>
2025-02-11 09:25:58 -0800

    updated files

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open('requirements.txt') as f:
 
 setup(
     name='scripts',
-    version='0.1.0',
-    author='Richard Yin,  Kenneth Weber, and Eddo Wesselink',  # Add another author's name
-    description='An imaging toolbox for muscle.',
+    version='0.2',
+    authors='Kenneth Weber, Eddo Wesselink, Benjamin DeLeener,' 'Richard Yin',  # Add authors' names
+    description='A toolbox for muscle imaging.',
     url='https://github.com/MuscleMap/MuscleMap.git',
     packages=find_packages(),
     install_requires=required,
@@ -16,11 +16,12 @@ setup(
         'console_scripts': [
             'mm_segment=scripts.mm_segment:main',
             'mm_extract_metrics=scripts.mm_extract_metrics:main',
-            'mm_gui=scripts.mm_gui:main' 
+            'mm_gui=scripts.mm_gui:main',
+            'mm_register_to_template=scripts.mm_register_to_template:main' 
         ]
     },
     package_data={
-        'scripts': ['models/**/*.json', 'models/**/*.pth'],
+        'scripts': ['models/**/*.json', 'models/**/*.pth', 'templates/**/*nii.gz'],
     },
     include_package_data=True,
     python_requires='>=3.6',
